739a83c8388d9a5657941cb3074d5e7c1fb67792
Ridwan Kazi <ridwank58@kprschools.ca>
2026-04-07 00:13:35 +0000

    Changes to app.py

--- app.py
+++ app.py
@@ -12,3 +12,43 @@ Please choose one of these options:
  
 Your selection:"""
 
+def menu():
+    connection = database.connect()
+    database.create_tables(connection)
+ 
+    while (user_input := input(MENU_PROMPT)) != "5":
+        if user_input == "1":
+            prompt_add_new_game(connection)
+        elif user_input == "2":
+            prompt_see_all_games(connection)
+        elif user_input == "3":
+            prompt_find_game(connection)
+        elif user_input == "4":
+            prompt_find_best_platform(connection)
+        else:
+            print("Invalid input, please try again.")
+ 
+def prompt_add_new_game(connection):
+    name = input("Enter game name: ")
+    platform = input("Enter the platform you played it on: ")
+    rating = int(input("Enter your rating score (0-100): "))
+    database.add_game(connection, name, platform, rating)
+ 
+def prompt_see_all_games(connection):
+    games = database.get_all_games(connection)
+    for game in games:
+        print(f"{game[1]} ({game[2]}) - {game[3]}/100")
+ 
+def prompt_find_game(connection):
+    name = input("Enter game name to find: ")
+    games = database.get_games_by_name(connection, name)
+    for game in games:
+        print(f"{game[1]} ({game[2]}) - {game[3]}/100")
+ 
+def prompt_find_best_platform(connection):
+    name = input("Enter game name to find: ")
+    best_platform = database.get_best_platform_for_game(connection, name)
+    print(f"The best platform for {name} is: {best_platform[2]}")
+ 
+ 
+menu()

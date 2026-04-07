fe534cd41f5356c51a0f38f54b614b0945193027
Ridwan Kazi <ridwank58@kprschools.ca>
2026-04-07 00:03:46 +0000

    Changes to app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import database
+
+MENU_PROMPT = """ -- Video Game Tracker --
+ 
+Please choose one of these options:
+ 
+1) Add a new game
+2) See all games.
+3) Find a game by name.
+4) See which platform is best for a game.
+5) Exit
+ 
+Your selection:"""
+

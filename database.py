ea654edeca2500ead89aff8dd858a298ab7e6329
Ridwan Kazi <ridwank58@kprschools.ca>
2026-04-07 00:02:54 +0000

    Changes to database.py

--- database.py
+++ database.py
@@ -11,3 +11,26 @@ SELECT * FROM games
 WHERE name = ?
 ORDER BY rating DESC
 LIMIT 1;"""
+
+def connect():
+    return sqlite3.connect("data.db")
+ 
+def create_tables(connection):
+    with connection:
+        connection.execute(CREATE_GAMES_TABLE)
+ 
+def add_game(connection, name, platform, rating):
+    with connection:
+        connection.execute(INSERT_GAME, (name, platform, rating))
+ 
+def get_all_games(connection):
+    with connection:
+        return connection.execute(GET_ALL_GAMES).fetchall()
+ 
+def get_games_by_name(connection, name):
+    with connection:
+        return connection.execute(GET_GAMES_BY_NAME, (name,)).fetchall()
+ 
+def get_best_platform_for_game(connection, name):
+    with connection:
+        return connection.execute(GET_BEST_PLATFORM_FOR_GAME, (name,)).fetchone()
